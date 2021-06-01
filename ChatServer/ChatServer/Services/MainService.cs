de5e6f375e320b4e4c96fb983162a4a8480b8587
Radu Alexandru <alexx15raduu@gmail.com>
2021-06-01 17:33:58 +0300

    Added logger for server-side activities from clients.

--- ChatServer/ChatServer/Services/MainService.cs
+++ ChatServer/ChatServer/Services/MainService.cs
@@ -12,31 +12,45 @@ namespace ChatServer.Services
 {
     public class MainService : Chat.ChatBase
     {
+        private readonly ILogger<MainService> _logger;
+        public MainService(ILogger<MainService> logger)
+        {
+            _logger = logger;
+        }
         public override Task<Empty> Login(UserRequest request, ServerCallContext context)
         {
-            ChatData.QueueMessage.messages.Add(new ChatData.Message 
-            { Id = new Random().Next(0, int.MaxValue), 
+            _logger.LogInformation($"\n[{DateTime.Now}]{request.User} has connected!\n");
+            ChatData.QueueMessage.messages.Add(new ChatData.Message
+            {
+                Id = new Random().Next(0, int.MaxValue),
                 Username = "SERVER",
-                UserMessage = $"{request.User} has connected!" }
+                UserMessage = $"{request.User} has connected!"
+            }
             );
             return Task.FromResult(new Empty());
         }
         public override Task<Empty> Logout(UserRequest request, ServerCallContext context)
         {
-            ChatData.QueueMessage.messages.Add(new ChatData.Message 
-            { Id = new Random().Next(0, int.MaxValue), 
+            _logger.LogInformation($"\n[{DateTime.Now}]{request.User} has disconnected!\n");
+            ChatData.QueueMessage.messages.Add(new ChatData.Message
+            {
+                Id = new Random().Next(0, int.MaxValue),
                 Username = "SERVER",
-                UserMessage = $"{request.User} has disconnected!" }
+                UserMessage = $"{request.User} has disconnected!"
+            }
             );
             return Task.FromResult(new Empty());
         }
 
         public override Task<Empty> SendMessage(MessageInput request, ServerCallContext context)
         {
-            ChatData.QueueMessage.messages.Add(new ChatData.Message 
-            { Id = new Random().Next(0, int.MaxValue),
+            _logger.LogInformation($"\n[{DateTime.Now}]{request.User}: {request.Message}\n");
+            ChatData.QueueMessage.messages.Add(new ChatData.Message
+            {
+                Id = new Random().Next(0, int.MaxValue),
                 Username = request.User,
-                UserMessage = request.Message }
+                UserMessage = request.Message
+            }
             );
             return Task.FromResult(new Empty());
         }
