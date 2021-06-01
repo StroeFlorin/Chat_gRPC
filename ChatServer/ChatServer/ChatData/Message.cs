43e666b856f890948b695511213b3f32df9346b4
florin <florinstroe2020@gmail.com>
2021-06-01 17:02:43 +0300

    Created a Message class which holds the message data. Created a list for messages history.

---
+++ ChatServer/ChatServer/ChatData/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.ChatData
+{
+    public class Message
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public String Username
+        {
+            get; set;
+        }
+        public String UserMessage
+        {
+            get; set;
+        }
+    }
+}
---
+++ ChatServer/ChatServer/ChatData/QueueMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.ChatData
+{
+    public static class QueueMessage
+    {
+        public static List<Message> messages = new List<Message>();
+    }
+}
