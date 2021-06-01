87096cfa0b8cdc9b41b3c4f881e33a425a1e0c40
Radu Alexandru <alexx15raduu@gmail.com>
2021-06-01 17:14:57 +0300

    Implemented Send messages feature.

--- ChatServer/ChatServer/Services/MainService.cs
+++ ChatServer/ChatServer/Services/MainService.cs
@@ -14,12 +14,30 @@ namespace ChatServer.Services
     {
         public override Task<Empty> Login(UserRequest request, ServerCallContext context)
         {
-            ChatData.QueueMessage.messages.Add(new ChatData.Message { Id = new Random().Next(0, int.MaxValue), Username = "SERVER", UserMessage = $"{request.User} has connected!" });
+            ChatData.QueueMessage.messages.Add(new ChatData.Message 
+            { Id = new Random().Next(0, int.MaxValue), 
+                Username = "SERVER",
+                UserMessage = $"{request.User} has connected!" }
+            );
             return Task.FromResult(new Empty());
         }
         public override Task<Empty> Logout(UserRequest request, ServerCallContext context)
         {
-            ChatData.QueueMessage.messages.Add(new ChatData.Message { Id = new Random().Next(0, int.MaxValue), Username = "SERVER", UserMessage = $"{request.User} has disconnected!" });
+            ChatData.QueueMessage.messages.Add(new ChatData.Message 
+            { Id = new Random().Next(0, int.MaxValue), 
+                Username = "SERVER",
+                UserMessage = $"{request.User} has disconnected!" }
+            );
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> SendMessage(MessageInput request, ServerCallContext context)
+        {
+            ChatData.QueueMessage.messages.Add(new ChatData.Message 
+            { Id = new Random().Next(0, int.MaxValue),
+                Username = request.User,
+                UserMessage = request.Message }
+            );
             return Task.FromResult(new Empty());
         }
     }
